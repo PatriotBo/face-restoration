d86f52d3d558d5af3dc0514baf98d0d030801137
joshuozhang <myshaobo11@163.com>
2023-06-18 12:22:24 +0800

    图片上传接口,绕过wx.uploadFile，直接提供服务

--- internal/logic/predict_handler.go
+++ internal/logic/predict_handler.go
@@ -2,9 +2,11 @@ package logic
 
 import (
 	"encoding/json"
-	"fmt"
-
 	"face-restoration/internal/constdata"
+	"fmt"
+	"io"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +16,31 @@ type PredictResponse struct {
 }
 
 func (m *MiniProgramImpl) Predict(ctx *gin.Context) {
+	filename := fmt.Sprintf("temp_%d.png", time.Now().UnixMilli())
+
+	out, err := os.Create("../images/" + filename)
+	if err != nil {
+		fmt.Printf("predict create file err:%v \n", err)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, ctx.Request.Body)
+	if err != nil {
+		fmt.Printf("predict copy file data err:%v \n", err)
+		return
+	}
+
+	resp := &PredictResponse{
+		ImageURL: fmt.Sprintf("%s/.%s", "", filename),
+	}
+	body, _ := json.Marshal(resp)
+	if _, err = ctx.Writer.Write(body); err != nil {
+		fmt.Printf("write response err:%v \n", err)
+	}
+}
+
+func (m *MiniProgramImpl) handleUploadFile(ctx *gin.Context) {
 	_, header, err := ctx.Request.FormFile("file")
 	if err != nil {
 		fmt.Printf("predict bad request err:%v \n", err)
@@ -25,13 +52,5 @@ func (m *MiniProgramImpl) Predict(ctx *gin.Context) {
 	if err = ctx.SaveUploadedFile(header, constdata.ImagePath); err != nil {
 		fmt.Printf("predict save field err:%v \n", err)
 		return
-	}
-
-	resp := &PredictResponse{
-		ImageURL: fmt.Sprintf("%s/.%s", "", filename),
-	}
-	body, _ := json.Marshal(resp)
-	if _, err = ctx.Writer.Write(body); err != nil {
-		fmt.Printf("write response err:%v \n", err)
 	}
 }
